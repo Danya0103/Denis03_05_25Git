1bd3ff3e321ff76296e411e1d83b83c1603d9d70
Danya0103 <user@WIN-LRBB7CDB9IA>
2025-05-03 13:51:18 +0300

    Bank

--- Denis03_05_25Git/Program.cs
+++ Denis03_05_25Git/Program.cs
@@ -1,5 +1,27 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main(string[] args)
+    {
+        double balance = 100;
+        Console.WriteLine($"{balance}");
+        Console.WriteLine($"{AddMoney(ref balance, 200.0)}");
+        Console.WriteLine($"{GiftMoney(ref balance, 50.0)}");
+        Console.WriteLine($"{balance}");
 
-// ds comment
+
+
+    }
+
+    static double AddMoney(ref double ba, double money)
+    {
+        ba += money;
+
+        return ba;
+    }
+
+    static double GiftMoney(ref double ba, double money)
+    {
+        ba -= money;
+
+        return ba;
+    }
+}
